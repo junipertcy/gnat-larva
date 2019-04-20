02e18fef36d130d166d8c9be15ca382b8059ebdb
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2019-04-20 16:23:29 -0600

    added txt directory

--- gnat_larva/nb.py
+++ gnat_larva/nb.py
@@ -15,6 +15,18 @@ def get_neighbors(particles, r, x0, y0):
 
     return neighbors
 
+def get_neighbors_3d(particles, r, x0, y0, z0):
+
+	neighbors = []
+
+	for j,(x1,y1,z1) in enumerate(particles):
+		dist = euclidean_distance(x0, y0, z0, x1, y1, z1)
+
+		if dist < r:
+			neighbors.append(j)
+
+	return neighbors
+
 
 # average unit vectors for all angles
 # return average angle
